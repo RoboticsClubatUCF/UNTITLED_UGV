a0c99ef8e2aad29e7749daf55efeecd16ed23808
Tevin M <deskcomp10@outlook.com>
2022-01-19 22:23:10 -0500

    creates noisy version of odometry topic

--- ugv_sim/scripts/add_noise.py
+++ ugv_sim/scripts/add_noise.py
@@ -1,92 +1,42 @@
-"""The purpose of this program is to simulate real-world sensor output by adding
-   adding noise to the sensor gazebo simulator sensor outputs."""
+"""The purpose of this script is to mimic real-world noisy sensor output"""
 
-import random as rand
 import rospy
-import math
+import random as rand
 from nav_msgs.msg import Odometry
 
-class Noise:
+class NoisyOdom():
 	def __init__(self):
-		#Normal distribution inits. Initialize as std. normal distribution
-		self.mean = 0 
-		self.deviation = 1
+		# Subscribe to the odometry topic 
+		self.odometry_subscriber = rospy.Subscriber('/bowser2/odom', Odometry, self.odom_callback)
+		# Publish a noisy odometry topic
+		self.noisy_odom_publisher = rospy.Publisher('/noisy_odom', Odometry, queue_size = 1)
+		# Create an odometry message
+		self.odometry_msg = Odometry()
+		# Set the publisher's loop frequency
+		self.rate = rospy.Rate(5) # 5Hz
 
-	def app_normal(self, quiet_del_x, quiet_del_y, dev):
-		#Set the deviation
-		self.deviation = dev
-		#Apply some noise onto the del values 
-		noisy_del_x = quiet_del_x + rand.gauss(self.mean, self.deviation)
-		noisy_del_y = quiet_del_y + rand.gauss(self.mean, self.deviation)
+	def odom_callback(self, msg):
+		# Save the odometry message from /bowser2/odom
+		self.odometry_msg = msg
+		# Call the add noise function
+		self.add_noise()
 
-		return noisy_del_x, noisy_del_y
+	def add_noise(self):
+		# Add noise to the X and Y position value of the odometry message
+		rand_float = rand.uniform(-0.5, 0.5) 
+		self.odometry_msg.pose.pose.position.x = self.odometry_msg.pose.pose.position.x + rand_float
+		self.odometry_msg.pose.pose.position.x = self.odometry_msg.pose.pose.position.y + rand_float
 
-class Topic_callbacks:
-	def __init__(self):
-		#Odometry inits
-		self.prev_x = 0
-		self.prev_y = 0
-		self.prev_noisy_x = 0
-		self.prev_noisy_y = 0
-		self.first_time = True
-		self.noise_multiplier = 0.00001
-
-	def odometry_cb(self, msg):
-		#Get the x and y position of the robot in the simulation
-		x = msg.pose.pose.position.x
-		y = msg.pose.pose.position.y
-
-		#Set the initial previous noisy x and y to the current x and y
-		#if this is the first time the callback function has been called
-		#Makes sure that the noisy odom and original odom have the same start
-		#position. Otherwise, the position for noisy will start at zero
-		if self.first_time:
-			self.prev_noisy_x = x
-			self.prev_noisy_y = y
-			self.first_time = False
-
-		#Determine how much the position of the robot has changed since
-		#the last call of this function
-		del_x = self.prev_x - x
-		self.prev_x = x #Assign the previous value of x the current value of x
-		del_y = self.prev_y - y
-		self.prev_y = y
-
-		#Apply a random number based on a normal distribution with the set deviation from a mean of 0
-		noisy_del_x, noisy_del_y = noise.app_normal(del_x, del_y, (self.noise_multiplier*((del_x+del_y)/2)))
-
-		#Keep everything in the original topic message
-		odometry = msg
-
-		#Determine the x and y positions given the noise.
-		#Because we are adding the noise to the previous x and y, the noisy_odom
-		#topic will always be one step behind the /bowser2/odom topic
-		odometry.pose.pose.position.x = self.prev_noisy_x + noisy_del_x
-		odometry.pose.pose.position.y = self.prev_noisy_y + noisy_del_y
-
-		#Reassign the previous noisy z and y values
-		self.prev_noisy_x = self.prev_noisy_x + noisy_del_x
-		self.prev_noisy_y = self.prev_noisy_y + noisy_del_y
-
-		#publish the data to the noisy_odom topic
-		noise_pub.publish(odometry)
-
-
-tpc = Topic_callbacks()
-noise = Noise()
-
-odometry = Odometry()
-odometry.pose.pose.position.x = 0
-odometry.pose.pose.position.y = 0
+	def publish_noisy_odom(self):
+		while not rospy.is_shutdown():
+			self.noisy_odom_publisher.publish(self.odometry_msg) # Publish noisy odom message to /noisy_odom topic
+			self.rate.sleep() # Sleep the required time to meet loop frequency set previsously
 
 if __name__ == "__main__":
-	rospy.init_node('my_odom') #Initialize a node of function argument name
-	rospy.Subscriber('/bowser2/odom', Odometry, tpc.odometry_cb) #Subscribe to the robot's odometry topic
-	noise_pub = rospy.Publisher('/bowser2/noisy_odom', Odometry, queue_size = 100)
-	rospy.spin()
-
-#mu = 0
-#sigma = 0.1
-
-#for _ in range(10):
-#	print(rand.gauss(mu, sigma))
+	# Create a node
+	rospy.init_node("noisy_odometry_node")
+	noisy_odom = NoisyOdom()
+	try:
+		noisy_odom.publish_noisy_odom()
+	except rospy.ROSInterruptException:
+		pass
